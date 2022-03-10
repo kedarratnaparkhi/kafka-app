7fca507951fd36170153d7e482f09d8a695e2d57
kedar <kedar.ratnaparkhi.dev@gmail.com>
2022-03-10 07:18:35 +0000

    Implement Kafka Consumer and Producer

--- src/main/java/org/kedar/kafka/SimpleConsumer.java
+++ src/main/java/org/kedar/kafka/SimpleConsumer.java
@@ -1,4 +1,43 @@
 package org.kedar.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
 public class SimpleConsumer {
+
+    SimpleConsumer(){
+
+        // Boilerplate Properties
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group");
+
+        // Create Consumer client
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // Subscribe to topic(s)
+        consumer.subscribe(Arrays.asList("messenger"));
+
+        // Poll and consume records
+        while(true){
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+            for(ConsumerRecord record: records){
+                System.out.println("Topic: "+record.topic()+" Key: "+record.key()+" Value: "+record.value());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SimpleConsumer consumer = new SimpleConsumer();
+    }
 }
--- src/main/java/org/kedar/kafka/SimpleProducer.java
+++ src/main/java/org/kedar/kafka/SimpleProducer.java
@@ -1,4 +1,29 @@
 package org.kedar.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
 public class SimpleProducer {
+
+    SimpleProducer(){
+        Properties properties = new Properties();
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+
+        ProducerRecord record = new ProducerRecord("messenger", "name", "hello, yoo!");
+
+        KafkaProducer producer = new KafkaProducer(properties);
+        producer.send(record);
+        producer.close();
+    }
+
+    public static void main(String[] args) {
+        SimpleProducer producer = new SimpleProducer();
+    }
 }
